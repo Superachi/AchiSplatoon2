9d417bfddfe01242d593e82f3d216480ef5a55f4
PascalKoeimanHHS <psclkmn@gmail.com>
2024-06-11 08:39:38 +0200

    Add BaseProjectile class, which every WoomyMod weapon projectile should inherit from

---
+++ Content/Projectiles/BaseProjectile.cs
@@ -0,0 +1,24 @@
+﻿using AchiSplatoon2.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace AchiSplatoon2.Content.Projectiles
+{
+    internal class BaseProjectile : ModProjectile
+    {
+        protected InkColor inkColor = InkColor.Blue;
+        protected bool isVisible = false;
+        protected float delayUntilVisible = 0f;
+
+        public void Initialize(InkColor color, bool visible, float visibleDelay)
+        {
+            inkColor = color;
+            isVisible = visible;
+            delayUntilVisible = visibleDelay;
+        }
+    }
+}
