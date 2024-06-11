d78c898b7b64a33f825f27200ab5ddb19332dac2
PascalKoeimanHHS <psclkmn@gmail.com>
2024-06-11 10:02:12 +0200

    Added BaseWeapon class to make other weapons inherit from

---
+++ Content/Items/Weapons/BaseWeapon.cs
@@ -0,0 +1,29 @@
+using AchiSplatoon2.Content.Projectiles;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AchiSplatoon2.Content.Items.Weapons
+{
+    // This is a basic item template.
+    // Please see tModLoader's ExampleMod for every other example:
+    // https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
+    public class BaseWeapon : ModItem
+    {
+        protected virtual float MuzzleOffsetPx { get; set; } = 0f;
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            Vector2 weaponOffset = HoldoutOffset() ?? new Vector2(0, 0);
+            Vector2 muzzleOffset = Vector2.Add(Vector2.Normalize(velocity) * MuzzleOffsetPx, Vector2.Normalize(velocity) * weaponOffset);
+
+            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+            {
+                position += muzzleOffset;
+            }
+        }
+    }
+}
