aecd818e46fde3c445a3218b01dd6f6fa31f0683
PascalKoeimanHHS <psclkmn@gmail.com>
2024-06-26 10:33:06 +0200

    Add Z+F charger and adjust charger sprites

---
+++ Content/Items/Weapons/Chargers/ZFSplatCharger.cs
@@ -0,0 +1,32 @@
+﻿using AchiSplatoon2.Content.Projectiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace AchiSplatoon2.Content.Items.Weapons.Chargers
+{
+    internal class ZFSplatCharger : SplatCharger
+    {
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Item.DefaultToRangedWeapon(ModContent.ProjectileType<SplatChargerProjectile>(), AmmoID.None, 15, 12f);
+            Item.damage = 360;
+            Item.knockBack = 5;
+            Item.value = Item.buyPrice(gold: 15);
+            Item.rare = ItemRarityID.LightPurple;
+        }
+
+        public override void AddRecipes()
+        {
+            var recipe = AddRecipeWithSheldonLicenseSilver(registerNow: false);
+            recipe.AddIngredient(ItemID.OrichalcumBar, 5);
+            recipe.Register();
+        }
+    }
+}
