15441a936f6f361740e29940e8ab3c0935ff7346
PascalKoeimanHHS <psclkmn@gmail.com>
2024-07-12 20:45:30 +0200

    Add mod items to some shops and add custom condition to check whether the evil bosses have been killed

---
+++ Content/CustomConditions/BossConditions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace AchiSplatoon2.Content.CustomConditions
+{
+    internal class BossConditions
+    {
+        public static readonly Condition DownedEvilBoss = new("Conditions.DownedEvilBoss", () => Condition.DownedEaterOfWorlds.IsMet() || Condition.DownedBrainOfCthulhu.IsMet());
+    }
+}
---
+++ Content/GlobalNPCs/ShopGlobalNPC.cs
@@ -0,0 +1,34 @@
+﻿using AchiSplatoon2.Content.CustomConditions;
+using AchiSplatoon2.Content.Items.Weapons.Bows;
+using AchiSplatoon2.Content.Items.Weapons.Brushes;
+using AchiSplatoon2.Content.Items.Weapons.Chargers;
+using AchiSplatoon2.Content.Items.Weapons.Shooters;
+using AchiSplatoon2.Content.Items.Weapons.Throwing;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AchiSplatoon2.Content.GlobalNPCs
+{
+    internal class ShopGlobalNPC : BaseGlobalNPC
+    {
+        public override void ModifyShop(NPCShop shop)
+        {
+            if (shop.NpcType == NPCID.Demolitionist)
+            {
+                shop.Add<SplatBomb>(BossConditions.DownedEvilBoss);
+                shop.Add<BurstBomb>();
+                shop.Add<AngleShooter>(Condition.DownedEarlygameBoss);
+                shop.Add<Sprinkler>(Condition.DownedEarlygameBoss);
+            }
+
+            if (shop.NpcType == NPCID.Merchant)
+            {
+                shop.Add<OrderShot>();
+                shop.Add<OrderBrush>();
+                shop.Add<OrderStringer>();
+                shop.Add<OrderCharger>();
+            }
+        }
+    }
+}
