c57b0c1f756b570badd5716f36149b591bff95de
PascalKoeimanHHS <psclkmn@gmail.com>
2024-07-13 10:31:22 +0200

    Add InputHelper class to check player input

---
+++ Helpers/InputHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria.GameInput;
+
+namespace AchiSplatoon2.Helpers
+{
+    internal class InputHelper
+    {
+        public static Vector2 GetInputVector()
+        {
+            // Source: tModLoader discord -> https://discord.com/channels/103110554649894912/534215632795729922/1239780417066762260
+            Vector2 result = Vector2.Zero;
+
+            result.X = GetInputX();
+            result.Y = GetInputY();
+            result.Normalize();
+
+            return result;
+        }
+
+        public static int GetInputX()
+        {
+            return PlayerInput.Triggers.Current.Right ? 1 : PlayerInput.Triggers.Current.Left ? -1 : 0;
+        }
+
+        public static int GetInputY()
+        {
+            return PlayerInput.Triggers.Current.Up ? 1 : PlayerInput.Triggers.Current.Down ? -1 : 0;
+        }
+
+        public static int GetInputXPressed()
+        {
+            return PlayerInput.Triggers.JustPressed.Right ? 1 : PlayerInput.Triggers.JustPressed.Left ? -1 : 0;
+        }
+
+        public static bool GetInputJumpPressed()
+        {
+            return PlayerInput.Triggers.JustPressed.Jump;
+        }
+    }
+}
