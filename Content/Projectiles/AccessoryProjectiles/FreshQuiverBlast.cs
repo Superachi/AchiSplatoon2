a778e3ac9928062c72ccde36d6f5658b2f55705f
PascalKoeimanHHS <psclkmn@gmail.com>
2024-07-14 12:57:43 +0200

    Set fresh quiver blast to ignore tile collision

--- Content/Projectiles/AccessoryProjectiles/FreshQuiverBlast.cs
+++ Content/Projectiles/AccessoryProjectiles/FreshQuiverBlast.cs
@@ -14,6 +14,7 @@ namespace AchiSplatoon2.Content.Projectiles.AccessoryProjectiles
             Projectile.friendly = true;
             Projectile.timeLeft = 6;
             Projectile.penetrate = -1;
+            Projectile.tileCollide = false;
         }
 
         public override void AfterSpawn()
