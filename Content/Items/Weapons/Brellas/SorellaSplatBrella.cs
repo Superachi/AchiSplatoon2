47136f4a99d1e68c8707047bbf98af4629903d61
PascalKoeimanHHS <psclkmn@gmail.com>
2024-07-27 09:51:24 +0200

    Add Sorella Splat Brella

---
+++ Content/Items/Weapons/Brellas/SorellaSplatBrella.cs
@@ -0,0 +1,32 @@
+﻿using AchiSplatoon2.Content.Projectiles.BrellaProjectiles;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace AchiSplatoon2.Content.Items.Weapons.Brellas
+{
+    internal class SorellaSplatBrella : SplatBrella
+    {
+        public override int ShieldLife => 400;
+        public override int ShieldCooldown => 450;
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+
+            Item.damage = 28;
+            Item.knockBack = 3;
+
+            Item.value = Item.buyPrice(gold: 20);
+            Item.rare = ItemRarityID.LightPurple;
+        }
+
+        public override void AddRecipes()
+        {
+            var recipe = AddRecipeWithSheldonLicenseSilver(registerNow: false);
+            recipe.AddIngredient(ItemID.SoulofFright, 5);
+            recipe.AddIngredient(ItemID.HallowedBar, 10);
+            recipe.Register();
+        }
+    }
+}
