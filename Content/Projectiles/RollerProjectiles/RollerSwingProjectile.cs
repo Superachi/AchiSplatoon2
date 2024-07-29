6af28e1492e0a50abc31155212c19c8364ffd8e9
PascalKoeimanHHS <psclkmn@gmail.com>
2024-07-29 19:14:38 +0200

    Reorder roller swing code. Reduce shot aim RNG and adjust damage calc when rolling/swinging. Also fix jitter when rolling and moving on a slope

--- Content/Projectiles/RollerProjectiles/RollerSwingProjectile.cs
+++ Content/Projectiles/RollerProjectiles/RollerSwingProjectile.cs
@@ -27,8 +27,10 @@ namespace AchiSplatoon2.Content.Projectiles.RollerProjectiles
 
         private int windUpTime = 20;
         private bool startedJumpSwing = false;
+        private float groundedWindUpTimeMod = 1f;
+        private float groundedSwingVelocityMod = 1f;
         private float jumpWindUpTimeMod = 1.5f;
-        private float jumpSwingVelocityMod = 2f;
+        private float jumpSwingVelocityMod = 1.5f;
         private float jumpSwingDamageMod = 1.5f;
 
         private int swingTime = 15;
@@ -37,6 +39,12 @@ namespace AchiSplatoon2.Content.Projectiles.RollerProjectiles
         private int facingDirection;
 
         Player owner;
+
+        protected float SwingAngleDegrees
+        {
+            get => Projectile.ai[1];
+            set => Projectile.ai[1] = value;
+        }
 
         public override void SetDefaults()
         {
@@ -51,12 +59,6 @@ namespace AchiSplatoon2.Content.Projectiles.RollerProjectiles
 
             //DrawOriginOffsetX = -Projectile.width / 2;
             //DrawOriginOffsetY = -Projectile.height / 2;
-        }
-
-        protected float SwingAngleDegrees
-        {
-            get => Projectile.ai[1];
-            set => Projectile.ai[1] = value;
         }
 
         public override void ApplyWeaponInstanceData()
@@ -89,144 +91,6 @@ namespace AchiSplatoon2.Content.Projectiles.RollerProjectiles
             AdvanceState();
         }
 
-        protected override void SetState(int targetState)
-        {
-            base.SetState(targetState);
-            stateTimer = 0;
-            switch (state)
-            {
-                case stateWindUp:
-                    if (startedJumpSwing)
-                    {
-                        startedJumpSwing = true;
-                        windUpTime = (int)(windUpTime * jumpWindUpTimeMod);
-                    }
-
-                    Projectile.friendly = false;
-                    PlayAudio(windUpSample, pitchVariance: 0.1f, maxInstances: 5);
-                    break;
-                case stateSwing:
-                    Projectile.friendly = true;
-                    PlayAudio(swingSample, pitchVariance: 0.1f, maxInstances: 5);
-                    break;
-                case stateRolling:
-                    break;
-            }
-        }
-
-        private bool IsPlayerGrounded()
-        {
-            var p = GetOwner();
-            return p.velocity.Y == 0 && p.oldVelocity.Y == 0;
-        }
-
-        private void RollerUpdateRotate()
-        {
-            // if (Projectile.friendly) VisualizeRadius();
-            Player p = GetOwner();
-
-            float deg = (SwingAngleDegrees);
-            float rad = MathHelper.ToRadians(deg);
-            float distanceFromPlayer = 48f;
-
-            Projectile.position.X = (p.Center.X) - (int)(Math.Cos(rad) * distanceFromPlayer) - Projectile.width / 2;
-            Projectile.position.Y = (p.Center.Y) - (int)(Math.Sin(rad) * distanceFromPlayer) - Projectile.height / 2;
-        }
-
-        private void RollerSwingRotate(float angleDestinationDegrees, float lerpAmount)
-        {
-            RollerUpdateRotate();
-            Player p = GetOwner();
-
-            // DebugHelper.PrintInfo($"{SwingAngleDegrees}");
-            SwingAngleDegrees = MathHelper.Lerp(SwingAngleDegrees, angleDestinationDegrees, lerpAmount);
-
-            var dirToPlayer = Projectile.Center
-                .DirectionTo(p.Center)
-                .ToRotation();
-            Projectile.rotation = dirToPlayer + MathHelper.ToRadians(45 + 180);
-        }
-
-        protected void StateWindUp()
-        {
-            if (facingDirection == 1) RollerSwingRotate(15, 0.15f);
-            else RollerSwingRotate(160, 0.15f);
-
-            if (stateTimer > windUpTime) AdvanceState();
-        }
-
-        private float VectorToDegrees(Vector2 vector)
-        {
-            var rad = vector.ToRotation();
-            return MathHelper.ToDegrees(vector.ToRotation());
-        }
-
-        private Vector2 DegreesToVector(float degrees)
-        {
-            return MathHelper.ToRadians(degrees).ToRotationVector2();
-        }
-
-        protected void StateSwing()
-        {
-            if (facingDirection == 1) RollerSwingRotate(200, 0.25f);
-            else RollerSwingRotate(-40, 0.15f);
-
-            if (stateTimer >= 2 && stateTimer < 6)
-            {
-                float velocityRandMod = 1f;
-                if (startedJumpSwing)
-                {
-                    velocityRandMod = 0.25f;
-                }
-                Player p = GetOwner();
-
-                var vecFromPlayer = Main.MouseWorld.DirectionFrom(p.Center);
-                float velocityMult = stateTimer + 2f;
-
-                Vector2 velocity = vecFromPlayer * velocityMult;
-                velocity += Main.rand.NextVector2Circular(1, 0.5f) * velocityRandMod;
-
-                // Make it so thrown projectiles always match the roller's direction,
-                // disregarding whether the player moves the mouse to the other side
-                if (Math.Sign(velocity.X) != facingDirection) velocity.X *= -1;
-
-                int damage = Projectile.damage;
-                if (startedJumpSwing)
-                {
-                    damage = (int)(Projectile.damage * jumpSwingDamageMod);
-                    velocity *= jumpSwingVelocityMod;
-                }
-                CreateChildProjectile<RollerInkProjectile>(p.Center, velocity, damage);
-
-                if (Main.rand.NextBool(3))
-                {
-                    velocity += Main.rand.NextVector2Circular(1, 0.5f) * velocityRandMod;
-                    CreateChildProjectile<RollerInkProjectile>(p.Center, velocity, damage);
-                }
-            }
-
-            if (stateTimer > swingTime) AdvanceState();
-        }
-
-        protected void StateRolling()
-        {
-            if (!InputHelper.GetInputMouseLeftHold())
-            {
-                Projectile.Kill();
-                return;
-            }
-
-            RollerUpdateRotate();
-            var p = GetOwner();
-            if (p.velocity.X != 0)
-            {
-                Projectile.friendly = true;
-            } else
-            {
-                Projectile.friendly = false;
-            }
-        }
-
         public override void AI()
         {
             Player owner = GetOwner();
@@ -248,6 +112,163 @@ namespace AchiSplatoon2.Content.Projectiles.RollerProjectiles
 
             owner.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, Projectile.rotation - MathHelper.ToRadians(90f));
             Vector2 armPosition = owner.GetFrontHandPosition(Player.CompositeArmStretchAmount.Full, Projectile.rotation - (float)Math.PI / 2);
+        }
+
+        public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
+        {
+            base.ModifyHitNPC(target, ref modifiers);
+            if (state == stateRolling)
+            {
+                var playerVelocity = Math.Abs(owner.velocity.X);
+                DebugHelper.PrintInfo($"{playerVelocity}");
+
+                if (playerVelocity > 2)
+                {
+                    modifiers.FinalDamage *= 0.5f + playerVelocity / 2;
+                    modifiers.Knockback += playerVelocity / 2;
+                }
+                else
+                {
+                    modifiers.FinalDamage *= 0.5f;
+                }
+            } else
+            {
+                modifiers.FinalDamage *= 2;
+            }
+
+            modifiers.HitDirectionOverride = Math.Sign(target.position.X - owner.position.X);
+        }
+
+        protected override void SetState(int targetState)
+        {
+            base.SetState(targetState);
+            stateTimer = 0;
+            switch (state)
+            {
+                case stateWindUp:
+                    if (startedJumpSwing)
+                    {
+                        windUpTime = (int)(windUpTime * jumpWindUpTimeMod);
+                    }
+
+                    Projectile.friendly = false;
+                    PlayAudio(windUpSample, pitchVariance: 0.1f, maxInstances: 5, pitch: -0.1f);
+                    break;
+                case stateSwing:
+                    Projectile.friendly = true;
+                    PlayAudio(swingSample, pitchVariance: 0.1f, maxInstances: 5);
+                    break;
+                case stateRolling:
+                    break;
+            }
+        }
+
+        protected void StateWindUp()
+        {
+            if (facingDirection == 1) RollerSwingRotate(15, 0.15f);
+            else RollerSwingRotate(160, 0.15f);
+
+            if (stateTimer > windUpTime) AdvanceState();
+        }
+
+        protected void StateSwing()
+        {
+            if (facingDirection == 1) RollerSwingRotate(200, 0.25f);
+            else RollerSwingRotate(-40, 0.15f);
+
+            if (stateTimer >= 2 && stateTimer < 6)
+            {
+                Player p = GetOwner();
+
+                var vecFromPlayer = Main.MouseWorld.DirectionFrom(p.Center);
+                float i = stateTimer - 2;
+                float velocityMult = 5f + i * (2 - i * 0.25f);
+                Vector2 velocity = vecFromPlayer * velocityMult;
+
+                // Make it so thrown projectiles always match the roller's direction,
+                // disregarding whether the player moves the mouse to the other side
+                if (Math.Sign(velocity.X) != facingDirection) velocity.X *= -1;
+
+                int damage = Projectile.damage;
+                if (startedJumpSwing)
+                {
+                    damage = (int)(Projectile.damage * jumpSwingDamageMod);
+                    velocity *= jumpSwingVelocityMod;
+                }
+                CreateChildProjectile<RollerInkProjectile>(p.Center, velocity, damage);
+                CreateChildProjectile<RollerInkProjectile>(p.Center, velocity * 0.75f, damage);
+            }
+
+            if (stateTimer > swingTime) AdvanceState();
+        }
+
+        protected void StateRolling()
+        {
+            if (!InputHelper.GetInputMouseLeftHold())
+            {
+                Projectile.Kill();
+                return;
+            }
+
+            RollerUpdateRotate();
+            var p = GetOwner();
+            if (p.velocity.X != 0)
+            {
+                Projectile.friendly = true;
+            }
+            else
+            {
+                Projectile.friendly = false;
+            }
+        }
+
+        private float VectorToDegrees(Vector2 vector)
+        {
+            var rad = vector.ToRotation();
+            return MathHelper.ToDegrees(vector.ToRotation());
+        }
+
+        private Vector2 DegreesToVector(float degrees)
+        {
+            return MathHelper.ToRadians(degrees).ToRotationVector2();
+        }
+
+        private bool IsPlayerGrounded()
+        {
+            var p = GetOwner();
+            return p.velocity.Y == 0 && p.oldVelocity.Y == 0;
+        }
+
+        private void RollerUpdateRotate()
+        {
+            // if (Projectile.friendly) VisualizeRadius();
+            Player p = GetOwner();
+            if (p.gfxOffY != 0)
+            {
+                DebugHelper.PrintWarning(p.gfxOffY);
+            }
+
+            float deg = (SwingAngleDegrees);
+            float rad = MathHelper.ToRadians(deg);
+            float distanceFromPlayer = 48f;
+
+            Projectile.gfxOffY = p.gfxOffY;
+            Projectile.position.X = (p.Center.X) - (int)(Math.Cos(rad) * distanceFromPlayer) - Projectile.width / 2;
+            Projectile.position.Y = (p.Center.Y) - (int)(Math.Sin(rad) * distanceFromPlayer) - Projectile.height / 2;
+        }
+
+        private void RollerSwingRotate(float angleDestinationDegrees, float lerpAmount)
+        {
+            RollerUpdateRotate();
+            Player p = GetOwner();
+
+            // DebugHelper.PrintInfo($"{SwingAngleDegrees}");
+            SwingAngleDegrees = MathHelper.Lerp(SwingAngleDegrees, angleDestinationDegrees, lerpAmount);
+
+            var dirToPlayer = Projectile.Center
+                .DirectionTo(p.Center)
+                .ToRotation();
+            Projectile.rotation = dirToPlayer + MathHelper.ToRadians(45 + 180);
         }
     }
 }
