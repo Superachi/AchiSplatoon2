b49cc34cc0507528247c937e15d7e0d203c1dec1
PascalKoeimanHHS <psclkmn@gmail.com>
2024-08-17 09:11:15 +0200

    Add InkSpawnPlayer (necessary for order bag to actually spawn in)

---
+++ Content/Players/InkSpawnPlayer.cs
@@ -0,0 +1,18 @@
+﻿using AchiSplatoon2.Content.Items.Consumables;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AchiSplatoon2.Content.Players
+{
+    internal class InkSpawnPlayer : BaseModPlayer
+    {
+        // Useful info: https://www.youtube.com/watch?v=K3MpL3r2mjQ&list=PL2j68jF83kP1ffowiMIwbTndwLgmxGFpd&index=10
+        public override IEnumerable<Item> AddStartingItems(bool mediumCoreDeath)
+        {
+            var starterItem = new Item();
+            starterItem.SetDefaults(ModContent.ItemType<OrderBag>(), false);
+            yield return starterItem;
+        }
+    }
+}
