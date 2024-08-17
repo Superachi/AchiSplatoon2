529d9c5ac4479058d82959ff8ea3a0a5db8aaa03
PascalKoeimanHHS <psclkmn@gmail.com>
2024-08-17 13:38:35 +0200

    add reusable function to spawn particles when rare item drops

--- Content/GlobalNPCs/LootGlobalNPC.cs
+++ Content/GlobalNPCs/LootGlobalNPC.cs
@@ -61,28 +61,15 @@ namespace AchiSplatoon2.Content.GlobalNPCs
                 if (Main.rand.NextBool((int)(100f * chanceModifier)))
                 {
                     Item.NewItem(npc.GetSource_Loot(), npc.Center, ModContent.ItemType<CannedSpecial>());
-
-                    // Display feedback if a canned special container drops
-                    for (int i = 0; i < 15; i++)
-                    {
-                        Dust dust;
-                        Vector2 position = npc.Center;
-                        Vector2 velocity = Main.rand.NextVector2Circular(15, 15);
-                        dust = Main.dust[
-                            Dust.NewDust(position, 0, 0, DustID.FireworksRGB, velocity.X, velocity.Y, 0, modPlayer.ColorFromChips, Main.rand.NextFloat(0.5f, 1.5f))
-                            ];
-                        dust.noGravity = true;
-                        dust.fadeIn = 1.5f;
-                    }
-
-                    SoundHelper.PlayAudio("ItemGet", volume: 0.6f, maxInstances: 1, position: npc.position);
+                    RareLootDropPlayerFeedback(npc);
                 }
 
                 // Sheldon license drop chance
-                if (Main.rand.NextBool((int)(1000f * chanceModifier)))
+                if (Condition.DownedEarlygameBoss.IsMet() && Main.rand.NextBool((int)(250f * chanceModifier)))
                 {
                     int licenseId = !Main.hardMode ? ModContent.ItemType<SheldonLicense>() : ModContent.ItemType<SheldonLicenseSilver>();
                     Item.NewItem(npc.GetSource_Loot(), npc.Center, licenseId);
+                    RareLootDropPlayerFeedback(npc);
                 }
 
                 // Sub weapon drop chance
@@ -105,6 +92,25 @@ namespace AchiSplatoon2.Content.GlobalNPCs
                     );
                 }
             }
+        }
+
+        private void RareLootDropPlayerFeedback(NPC npc)
+        {
+            var modPlayer = Main.LocalPlayer.GetModPlayer<InkWeaponPlayer>();
+
+            for (int i = 0; i < 15; i++)
+            {
+                Dust dust;
+                Vector2 position = npc.Center;
+                Vector2 velocity = Main.rand.NextVector2Circular(15, 15);
+                dust = Main.dust[
+                    Dust.NewDust(position, 0, 0, DustID.FireworksRGB, velocity.X, velocity.Y, 0, modPlayer.ColorFromChips, Main.rand.NextFloat(0.5f, 1.5f))
+                    ];
+                dust.noGravity = true;
+                dust.fadeIn = 1.5f;
+            }
+
+            SoundHelper.PlayAudio("ItemGet", volume: 0.6f, maxInstances: 1, position: npc.position);
         }
 
         public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
