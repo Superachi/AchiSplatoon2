873cfbb5f6adbdc0108c4e0ba90741a058d1bf67
PascalKoeimanHHS <psclkmn@gmail.com>
2024-08-24 12:10:31 +0200

    Properly add loot to treasure bag of wall of flesh and slime king

---
+++ Content/GlobalItems/GlobalTreasureBags.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using AchiSplatoon2.Content.Items.Accessories.Emblems;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ModLoader.IO;
+using AchiSplatoon2.Content.Items.Accessories.ColorChips;
+using AchiSplatoon2.Content.Items.Weapons.Throwing;
+using AchiSplatoon2.Content.Items.Weapons.Shooters;
+
+namespace AchiSplatoon2.Content.GlobalItems
+{
+    internal class GlobalTreasureBags : GlobalItem
+    {
+        public override void ModifyItemLoot(Item item, ItemLoot itemLoot)
+        {
+            if (item.type == ItemID.KingSlimeBossBag)
+            {
+                itemLoot.Add(
+                    ItemDropRule.Common(ModContent.ItemType<SlimeSplattershot>(), chanceDenominator: 5)
+                );
+            }
+
+            if (item.type == ItemID.WallOfFleshBossBag)
+            {
+                itemLoot.Add(
+                    ItemDropRule.OneFromOptions(
+                        1,
+                        ModContent.ItemType<SpecialChargeEmblem>(),
+                        ModContent.ItemType<SpecialPowerEmblem>(),
+                        ModContent.ItemType<SubPowerEmblem>()
+                    )
+                );
+
+                itemLoot.Add(
+                    ItemDropRule.OneFromOptions(
+                        5,
+                        ModContent.ItemType<SplooshOMatic>(),
+                        ModContent.ItemType<SplashOMatic>()
+                    )
+                );
+            }
+        }
+    }
+}
