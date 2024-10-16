c71cef8c864c9d2655acfd967409943e07be63d3
PascalKoeimanHHS <psclkmn@gmail.com>
2024-10-16 11:15:51 +0200

    Add order blaster/brella

---
+++ Content/Items/Weapons/Blasters/OrderBlaster.cs
@@ -0,0 +1,37 @@
+﻿using AchiSplatoon2.Content.Projectiles.BlasterProjectiles;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AchiSplatoon2.Content.Items.Weapons.Blasters
+{
+    internal class OrderBlaster : BaseBlaster
+    {
+        public override int ExplosionRadiusAir { get => 160; }
+        public override int ExplosionRadiusTile { get => 120; }
+        public override float ExplosionDelayInit { get => 10f; }
+
+        public override Vector2? HoldoutOffset() { return new Vector2(-10, -10); }
+        public override float MuzzleOffsetPx { get; set; } = 46f;
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            RangedWeaponDefaults(
+                projectileType: ModContent.ProjectileType<BlasterProjectileV2>(),
+                singleShotTime: 58,
+                shotVelocity: 6f);
+
+            Item.damage = 30;
+            Item.width = 42;
+            Item.height = 44;
+            Item.knockBack = 2;
+
+            Item.value = Item.buyPrice(silver: 10);
+            Item.rare = ItemRarityID.Blue;
+        }
+
+        public override void AddRecipes() => AddRecipeOrder();
+    }
+}
--- Content/Items/Weapons/Brellas/OrderBrella.cs
+++ Content/Items/Weapons/Brellas/OrderBrella.cs
@@ -2,15 +2,13 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework;
 
 namespace AchiSplatoon2.Content.Items.Weapons.Brellas
 {
-    internal class SplatBrella : BaseBrella
+    internal class OrderBrella : BaseBrella
     {
         public override int ProjectileCount { get => 4; }
-        public override int ShieldLife => 200;
+        public override int ShieldLife => 100;
         public override int ShieldCooldown => 450;
 
         public override void SetDefaults()
@@ -18,16 +16,19 @@ namespace AchiSplatoon2.Content.Items.Weapons.Brellas
             base.SetDefaults();
             RangedWeaponDefaults(
                 projectileType: ModContent.ProjectileType<BrellaShotgunProjectile>(),
-                singleShotTime: 36,
+                singleShotTime: 40,
                 shotVelocity: 8f);
 
-            Item.damage = 10;
+            Item.damage = 6;
             Item.width = 50;
             Item.height = 58;
-            Item.knockBack = 2;
+            Item.knockBack = 1;
 
-            Item.value = Item.buyPrice(gold: 3);
-            Item.rare = ItemRarityID.Green;
+            Item.value = Item.buyPrice(silver: 10);
+            Item.rare = ItemRarityID.Blue;
+
+            // Note: hide this stat from the player-- the Order Brella shouldn't be seen as a swapout for high-def enemies
+            Item.ArmorPenetration = 3;
         }
 
         public override void AddRecipes() => AddRecipePostBee();
