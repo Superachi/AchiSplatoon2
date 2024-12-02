74d34acf461fec0b452b61663944816d99343620
PascalKoeimanHHS <psclkmn@gmail.com>
2024-12-02 16:12:28 +0100

    Make heavy duty prefix a bit less drastic

--- Content/Prefixes/GeneralPrefixes/HeavyDutyPrefix.cs
+++ Content/Prefixes/GeneralPrefixes/HeavyDutyPrefix.cs
@@ -3,9 +3,9 @@
     internal class HeavyDutyPrefix : BaseWeaponPrefix
     {
         public override float PrefixValueModifier => 0.6f;
-        public override float DamageModifier => 1.2f;
+        public override float DamageModifier => 1f;
         public override float KnockbackModifier => 0.2f;
-        public override float UseTimeModifier => 1f;
+        public override float UseTimeModifier => 0.8f;
         public override float VelocityModifier => 0.2f;
         public override float InkCostModifier => 0.2f;
     }
