1e1208f5f485627157ad619f27c2399232c5ef4c
PascalKoeimanHHS <psclkmn@gmail.com>
2025-03-02 13:26:29 +0100

    Adjust painter dualie stats

--- Content/Items/Weapons/Dualies/PainterDualie.cs
+++ Content/Items/Weapons/Dualies/PainterDualie.cs
@@ -10,9 +10,10 @@ namespace AchiSplatoon2.Content.Items.Weapons.Dualies
 {
     internal class PainterDualie : BaseDualie
     {
-        public override float ShotGravity { get => 0.1f; }
-        public override int ShotGravityDelay { get => 12; }
-        public override int ShotExtraUpdates { get => 4; }
+        public override float InkCost { get => 2f; }
+        public override float ShotGravity { get => 0.2f; }
+        public override int ShotGravityDelay { get => 8; }
+        public override int ShotExtraUpdates { get => 5; }
         public override float AimDeviation { get => 8f; }
         public override SoundStyle ShootSample { get => SoundPaths.SplattershotShoot.ToSoundStyle(); }
         public override Vector2? HoldoutOffset() { return new Vector2(-8, 0); }
@@ -21,10 +22,10 @@ namespace AchiSplatoon2.Content.Items.Weapons.Dualies
         // Dualie specific
         public override SoundStyle RollSample { get => SoundPaths.DualieTetraRoll.ToSoundStyle(); }
         public override float RollInkCost { get => 3f; }
-        public override float PostRollDamageMod { get => 0.6f; }
-        public override float PostRollAttackSpeedMod { get => 0.4f; }
+        public override float PostRollDamageMod { get => 2f; }
+        public override float PostRollAttackSpeedMod { get => 1.5f; }
         public override float PostRollAimMod { get => 0.5f; }
-        public override float PostRollVelocityMod { get => 1f; }
+        public override float PostRollVelocityMod { get => 1.5f; }
         public override int MaxRolls { get => 1; }
         public override float RollDistance { get => 10f; }
         public override float RollDuration { get => 14f; }
@@ -35,10 +36,10 @@ namespace AchiSplatoon2.Content.Items.Weapons.Dualies
             base.SetDefaults();
             RangedWeaponDefaults(
                 projectileType: ModContent.ProjectileType<PainterDualieShotProjectile>(),
-                singleShotTime: 15,
+                singleShotTime: 10,
                 shotVelocity: 4f);
 
-            Item.damage = 9;
+            Item.damage = 8;
             Item.ArmorPenetration = 3;
 
             Item.width = 46;
