f29e4382357c23823c49a177db394120265c0fe4
PascalKoeimanHHS <psclkmn@gmail.com>
2025-03-16 10:44:32 +0100

    Adjust some prefixes (heavy duty is a bit faster/less ink hungry, resonant prefix is less expensive)

--- Content/Prefixes/SlosherPrefixes/ResonantPrefix.cs
+++ Content/Prefixes/SlosherPrefixes/ResonantPrefix.cs
@@ -2,7 +2,7 @@
 {
     internal class ResonantPrefix : BaseSlosherPrefix
     {
-        public override float PrefixValueModifier => 2f;
+        public override float PrefixValueModifier => 1.2f;
         public override float DamageModifier => 0f;
         public override int RepetitionBonus => 1;
         public override float VelocityModifier => -0.2f;
