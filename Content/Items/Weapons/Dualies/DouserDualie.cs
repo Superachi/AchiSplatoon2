41e2d0de2d951d05b0cd53749bc5eb639a3fe21f
PascalKoeimanHHS <psclkmn@gmail.com>
2025-04-05 22:22:40 +0200

    Adjust douser dualie: slightly lower RoF, increase damage, lower range

--- Content/Items/Weapons/Dualies/DouserDualie.cs
+++ Content/Items/Weapons/Dualies/DouserDualie.cs
@@ -11,9 +11,9 @@ namespace AchiSplatoon2.Content.Items.Weapons.Dualies
     {
         // Shoot settings
         public override float ShotGravity { get => 0.3f; }
-        public override int ShotGravityDelay { get => 20; }
+        public override int ShotGravityDelay { get => 15; }
         public override int ShotExtraUpdates { get => 4; }
-        public override float AimDeviation { get => 2f; }
+        public override float AimDeviation { get => 3f; }
         public override SoundStyle ShootAltSample { get => ShootSample; }
         public override Vector2? HoldoutOffset() { return new Vector2(0, 0); }
         public override float MuzzleOffsetPx { get; set; } = 52f;
@@ -21,7 +21,7 @@ namespace AchiSplatoon2.Content.Items.Weapons.Dualies
         // Dualie specific
         public override float RollInkCost { get => 8f; }
         public override float PostRollDamageMod { get => 1.2f; }
-        public override float PostRollAttackSpeedMod { get => 0.5f; }
+        public override float PostRollAttackSpeedMod { get => 0.55f; }
         public override float PostRollAimMod { get => 0.25f; }
         public override float PostRollVelocityMod { get => 0.5f; }
         public override int MaxRolls { get => 1; }
@@ -33,10 +33,10 @@ namespace AchiSplatoon2.Content.Items.Weapons.Dualies
             base.SetDefaults();
             RangedWeaponDefaults(
                 projectileType: ModContent.ProjectileType<DualieShotProjectile>(),
-                singleShotTime: 8,
+                singleShotTime: 9,
                 shotVelocity: 10f);
 
-            Item.damage = 20;
+            Item.damage = 26;
             Item.width = 52;
             Item.height = 34;
             Item.knockBack = 2;
