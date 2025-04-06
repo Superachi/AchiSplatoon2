f3299c953a9afa921db514e0d6c1f4508650f3d8
PascalKoeimanHHS <psclkmn@gmail.com>
2025-04-06 20:50:23 +0200

    Reduce princess prefix velocity

--- Content/Prefixes/DualiePrefixes/PrincessPrefix.cs
+++ Content/Prefixes/DualiePrefixes/PrincessPrefix.cs
@@ -4,6 +4,6 @@ internal class PrincessPrefix : BaseDualiePrefix
 {
     public override int ExtraDodgeRolls => 1;
     public override float UseTimeModifier => -0.1f;
-    public override float VelocityModifier => -0.1f;
+    public override float VelocityModifier => -0.2f;
     public override int CritChanceBonus => 5;
 }
