cd884d9b9295932b43af1f9a868cb528a0b0f654
PascalKoeimanHHS <psclkmn@gmail.com>
2025-05-15 14:29:45 +0200

    Adjust twin prefix stats (bit higher damage, removed charge speed bonus, added ink cost nerf)

--- Content/Prefixes/ChargerPrefixes/TwinPrefix.cs
+++ Content/Prefixes/ChargerPrefixes/TwinPrefix.cs
@@ -5,10 +5,10 @@
         public static float TwinShotArc => 8f;
 
         public override float PrefixValueModifier => 1.6f;
-        public override float DamageModifier => -0.3f;
+        public override float DamageModifier => -0.2f;
         public override float LifetimeModifier => -0.6f;
-        public override float ChargeSpeedModifier => 0.1f;
+        public override float ChargeSpeedModifier => 0f;
         public override int ExtraProjectileBonus => 1;
-        public override float InkCostModifier => 0f;
+        public override float InkCostModifier => 0.2f;
     }
 }
