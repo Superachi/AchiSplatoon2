3d6c211123096a24464739dc5877a8ddbeb0946e
PascalKoeimanHHS <psclkmn@gmail.com>
2025-05-23 18:18:20 +0200

    Nerf damage on turbo prefix

--- Content/Prefixes/GeneralPrefixes/TurboPrefix.cs
+++ Content/Prefixes/GeneralPrefixes/TurboPrefix.cs
@@ -3,7 +3,7 @@
 internal class TurboPrefix : BaseWeaponPrefix
 {
     public override float PrefixValueModifier => 2f;
-    public override float DamageModifier => -0.3f;
+    public override float DamageModifier => -0.4f;
     public override float UseTimeModifier => -0.4f;
     public override float InkCostModifier => -0.2f;
 }
