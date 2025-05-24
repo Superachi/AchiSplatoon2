6e01230c998492024b9d3e724f6933add2c672ee
PascalKoeimanHHS <psclkmn@gmail.com>
2025-05-24 10:37:39 +0200

    Adjust value on cheap prefix, slightly extend range on short-fused prefix

--- Content/Prefixes/BlasterPrefixes/ShortFusedPrefix.cs
+++ Content/Prefixes/BlasterPrefixes/ShortFusedPrefix.cs
@@ -5,7 +5,7 @@
         public override float PrefixValueModifier => 1.2f;
         public override float UseTimeModifier => -0.2f;
         public override float ExplosionRadiusModifier => -0.2f;
-        public override float LifetimeModifier => -0.5f;
+        public override float LifetimeModifier => -0.4f;
         public override float InkCostModifier => -0.2f;
     }
 }
--- Content/Prefixes/GeneralPrefixes/InkCostPrefixes/CheapPrefix.cs
+++ Content/Prefixes/GeneralPrefixes/InkCostPrefixes/CheapPrefix.cs
@@ -2,6 +2,7 @@
 {
     internal class CheapPrefix : BaseWeaponPrefix
     {
+        public override float PrefixValueModifier => 1.5f;
         public override float DamageModifier => -0.2f;
         public override float InkCostModifier => -0.5f;
     }
